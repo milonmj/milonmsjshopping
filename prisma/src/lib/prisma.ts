770a242d6c569752b4c8bd92696350e7a9a6d67c
milonmj <milonjibon069@gmail.com>
2026-09-05 17:38:35 +0400

    Delete prisma/src/lib/prisma.ts

--- prisma/src/lib/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// Prevents creating a new DB connection on every hot-reload in dev
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
