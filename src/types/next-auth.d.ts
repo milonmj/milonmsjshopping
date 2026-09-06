78dbbe68c07755ec4914ec6d9805783ad28f1902
milonmj <milonjibon069@gmail.com>
2026-09-06 20:40:10 +0400

    Update next-auth.d.ts
    field to next-auth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,11 +4,13 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      phone: string;
       role: string;
     } & DefaultSession["user"];
   }
 
   interface User {
+    phone: string;
     role: string;
   }
 }
@@ -16,6 +18,7 @@ declare module "next-auth" {
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
+    phone: string;
     role: string;
   }
 }
